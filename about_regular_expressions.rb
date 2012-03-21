2d815201b6a076b39292e317a231c0456f009578
Amanda <ms.amgilmore@gmail.com>
2012-03-21 16:59:13 -0400

    Amanda: worked up to regexp file

--- about_regular_expressions.rb
+++ about_regular_expressions.rb
@@ -27,13 +27,14 @@ class AboutRegularExpressions < EdgeCase::Koan
   end
 
   def test_asterisk_means_zero_or_more
-    assert_equal __, "abbcccddddeeeee"[/ab*/]
-    assert_equal __, "abbcccddddeeeee"[/az*/]
-    assert_equal __, "abbcccddddeeeee"[/z*/]
+    assert_equal "abb", "abbcccddddeeeee"[/ab*/]
+    assert_equal "a", "abbcccddddeeeee"[/az*/]
+    assert_equal "", "abbcccddddeeeee"[/z*/]
 
     # THINK ABOUT IT:
     #
     # When would * fail to match?
+    #when you pass it on nil?
   end
 
   # THINK ABOUT IT:
@@ -45,101 +46,107 @@ class AboutRegularExpressions < EdgeCase::Koan
   # ------------------------------------------------------------------
 
   def test_the_left_most_match_wins
-    assert_equal __, "abbccc az"[/az*/]
+    assert_equal "a", "abbccc az"[/az*/]
   end
 
   # ------------------------------------------------------------------
 
   def test_character_classes_give_options_for_a_character
     animals = ["cat", "bat", "rat", "zat"]
-    assert_equal __, animals.select { |a| a[/[cbr]at/] }
+    assert_equal ["cat", "bat", "rat"], animals.select { |a| a[/[cbr]at/] }
   end
 
   def test_slash_d_is_a_shortcut_for_a_digit_character_class
-    assert_equal __, "the number is 42"[/[0123456789]+/]
-    assert_equal __, "the number is 42"[/\d+/]
+    assert_equal "42", "the number is 42"[/[0123456789]+/]
+    assert_equal "42", "the number is 42"[/\d+/]
   end
 
   def test_character_classes_can_include_ranges
-    assert_equal __, "the number is 42"[/[0-9]+/]
+    assert_equal "2", "the number is 42"[/[0-3]+/]
+    #This was originally 
+    #assert_equal __, "the number is 42"[/[0-9]+/]
+    #but I was kind of curious what would happen if I included in the string numbers outside of the regex range 
   end
 
   def test_slash_s_is_a_shortcut_for_a_whitespace_character_class
-    assert_equal __, "space: \t\n"[/\s+/]
+    assert_equal " \t\n", "space: \t\n"[/\s+/]
   end
 
   def test_slash_w_is_a_shortcut_for_a_word_character_class
     # NOTE:  This is more like how a programmer might define a word.
-    assert_equal __, "variable_1 = 42"[/[a-zA-Z0-9_]+/]
-    assert_equal __, "variable_1 = 42"[/\w+/]
+    assert_equal "variable_1", "variable_1 = 42"[/[a-zA-Z0-9_]+/]
+    assert_equal "variable_1", "variable_1 = 42"[/\w+/]
   end
 
   def test_period_is_a_shortcut_for_any_non_newline_character
-    assert_equal __, "abc\n123"[/a.+/]
+    assert_equal "abc", "abc\n123"[/a.+/]
   end
 
   def test_a_character_class_can_be_negated
-    assert_equal __, "the number is 42"[/[^0-9]+/]
+    assert_equal "the number is ", "the number is 42"[/[^0-9]+/]
   end
 
   def test_shortcut_character_classes_are_negated_with_capitals
-    assert_equal __, "the number is 42"[/\D+/]
-    assert_equal __, "space: \t\n"[/\S+/]
+    assert_equal "the number is ", "the number is 42"[/\D+/]
+    assert_equal "space:", "space: \t\n"[/\S+/]
     # ... a programmer would most likely do
-    assert_equal __, "variable_1 = 42"[/[^a-zA-Z0-9_]+/]
-    assert_equal __, "variable_1 = 42"[/\W+/]
+    assert_equal " = ", "variable_1 = 42"[/[^a-zA-Z0-9_]+/]
+    assert_equal " = ", "variable_1 = 42"[/\W+/]
   end
 
   # ------------------------------------------------------------------
 
   def test_slash_a_anchors_to_the_start_of_the_string
-    assert_equal __, "start end"[/\Astart/]
-    assert_equal __, "start end"[/\Aend/]
+    #...but this is a capital a, why isn't it negative?   
+    assert_equal "start", "start end"[/\Astart/]
+    assert_equal nil, "start end"[/\Aend/]
   end
 
   def test_slash_z_anchors_to_the_end_of_the_string
-    assert_equal __, "start end"[/end\z/]
-    assert_equal __, "start end"[/start\z/]
+    assert_equal "end", "start end"[/end\z/]
+    assert_equal nil, "start end"[/start\z/]
   end
 
   def test_caret_anchors_to_the_start_of_lines
-    assert_equal __, "num 42\n2 lines"[/^\d+/]
+    assert_equal "2", "num 42\n2 lines"[/^\d+/]
+    #__ was expected but is 2...this is the END of the line?
   end
 
   def test_dollar_sign_anchors_to_the_end_of_lines
-    assert_equal __, "2 lines\nnum 42"[/\d+$/]
+    assert_equal "42", "2 lines\nnum 42"[/\d+$/]
   end
 
   def test_slash_b_anchors_to_a_word_boundary
-    assert_equal __, "bovine vines"[/\bvine./]
+    assert_equal "vines", "bovine vines"[/\bvine./]
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_group_contents
-    assert_equal __, "ahahaha"[/(ha)+/]
+    assert_equal "hahaha", "ahahaha"[/(ha)+/]
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_also_capture_matched_content_by_number
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 1]
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 2]
+    assert_equal "Gray", "Gray, James"[/(\w+), (\w+)/, 1]
+    assert_equal "James", "Gray, James"[/(\w+), (\w+)/, 2]
   end
 
   def test_variables_can_also_be_used_to_access_captures
-    assert_equal __, "Name:  Gray, James"[/(\w+), (\w+)/]
-    assert_equal __, $1
-    assert_equal __, $2
+    assert_equal "Gray, James", "Name:  Gray, James"[/(\w+), (\w+)/]
+    assert_equal "Gray", $1
+    assert_equal "James", $2
+    #dollar sign seeming to indicate a kind of number sign? not sure about this
   end
 
   # ------------------------------------------------------------------
 
   def test_a_vertical_pipe_means_or
     grays = /(James|Dana|Summer) Gray/
-    assert_equal __, "James Gray"[grays]
-    assert_equal __, "Summer Gray"[grays, 1]
-    assert_equal __, "Jim Gray"[grays, 1]
+    assert_equal "James Gray", "James Gray"[grays]
+    assert_equal "Summer", "Summer Gray"[grays, 1]
+    assert_equal nil, "Jim Gray"[grays, 1]
   end
 
   # THINK ABOUT IT:
@@ -149,7 +156,7 @@ class AboutRegularExpressions < EdgeCase::Koan
   # ------------------------------------------------------------------
 
   def test_scan_is_like_find_all
-    assert_equal __, "one two-three".scan(/\w+/)
+    assert_equal ["one", "two", "three"], "one two-three".scan(/\w+/)
   end
 
   def test_sub_is_like_find_and_replace
